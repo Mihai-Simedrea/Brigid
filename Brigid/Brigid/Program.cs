7285a2d800c1c1b95f315078add8517b9e1ab321
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2022-09-10 07:48:13 -0700

    Set up Swagger

--- Brigid/Brigid/Program.cs
+++ Brigid/Brigid/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +14,23 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    // Use Swagger
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = String.Empty;
+});
+
+app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
